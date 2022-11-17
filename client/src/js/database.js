7322c3cfd907f77d9d755319a29445fabddf9d5e
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-16 20:25:34 -0500

    all content goes to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get('content');
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({content: content});
+  const result = await request;
+  console.log('Thw data was saved to the database', result);
+  
+};
 
 initdb();

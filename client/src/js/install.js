ee1f4f38481b72198e6db98c75d6e64fea91b391
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-16 20:31:52 -0500

    last button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -27,4 +27,6 @@ butInstall.addEventListener("click", async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("appinstalled", event);
+});

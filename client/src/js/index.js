6ba9286300b7243de3525376555cdd5c21f908d6
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-16 20:12:04 -0500

    commit

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -24,10 +24,39 @@ if (typeof editor === 'undefined') {
 }
 
 // Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
-} else {
-  console.error('Service workers are not supported in this browser.');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(function (reg) {
+      console.log("Successfully registered!", reg);
+    })
+    .catch(function (err) {
+      console.warn("Error registering service worker", err);
+    });
 }
+
+// Logic for installing the PWA
+const butInstall = document.getElementById("buttonInstall");
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  deferredPrompt = e;
+  butInstall.style.visibility = "visible";
+});
+
+butInstall.addEventListener("click", async () => {
+  if (deferredPrompt !== null) {
+    deferredPrompt?.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      butInstall.setAttribute("disabled", true);
+      butInstall.textContent = "Installed!";
+      deferredPrompt = null;
+    }
+  }
+});
+
+window.addEventListener("appinstalled", (event) => {
+  console.log("appinstalled", event);
+});
